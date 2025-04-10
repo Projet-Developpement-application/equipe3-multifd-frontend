30ae1e3f9162be95ae5210ba86124fbcbe2beb9c
jaja <2264502@cegeplimoilou.ca>
2025-04-10 14:17:02 -0400

    Ajout d'un barre de navigation minimale

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,42 +1,30 @@
 export default function Navbar(){
     return (
-        <nav className="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
+        <nav className="navbar position-fixed w-100 z-1 navbar-expand-lg text-white bg-primary" data-bs-theme="dark">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">Navbar</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="true" aria-label="Toggle navigation">
+                <a className="navbar-brand  m-2" href="#">MULTI FD</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="navbar-collapse collapse show" id="navbarColor01" >
-                    <ul className="navbar-nav me-auto">
-                        <li className="nav-item">
-                            <a className="nav-link active" href="#">Home
-                                <span className="visually-hidden">(current)</span>
-                            </a>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav align-items-end ms-auto ">
+                        <li className="nav-item me-5">
+                            <button
+                                className="nav-link btn btn-navbar left pb-0 mb-2 " href="#"> Panier
+                            </button>
                         </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Features</a>
+                        <li className="nav-item me-5">
+                            <button
+                                className="nav-link btn btn-navbar left pb-0 mb-2" href="#"> Mon Compte
+                            </button>
                         </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Pricing</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">About</a>
-                        </li>
-                        <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-                            <div className="dropdown-menu">
-                                <a className="dropdown-item" href="#">Action</a>
-                                <a className="dropdown-item" href="#">Another action</a>
-                                <a className="dropdown-item" href="#">Something else here</a>
-                                <div className="dropdown-divider"></div>
-                                <a className="dropdown-item" href="#">Separated link</a>
-                            </div>
+
+                        <li className="nav-item me-3">
+                            <button className="nav-link btn btn-navbar " href="#"><i className="bi bi-github"> </i>
+                            </button>
                         </li>
                     </ul>
-                    <form className="d-flex">
-                        <input className="form-control me-sm-2" type="search" placeholder="Search"/>
-                            <button className="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
-                    </form>
                 </div>
             </div>
         </nav>)
