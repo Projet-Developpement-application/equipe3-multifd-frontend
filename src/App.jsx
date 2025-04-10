d31e187aba931f677167c453d451197c7afa1270
jaja <2264502@cegeplimoilou.ca>
2025-04-10 14:45:38 -0400

    Ajout du catalogue avec pagination

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import Navbar from "./components/navbar/Navbar.jsx";
+import Catalogue from "./components/catalogue/Catalogue.jsx";
 
 function App() {
 return(
+    <>
     <Navbar/>
-
+    <Catalogue/>
+    </>
 )
 }
 
