f2f4a5a780ec0349ba1b9c2fd9cc8fb0b9d44197
Janelle <2264502@cegeplimoilou.ca>
2025-04-15 17:15:07 -0400

    Ajout rapide formulaire panier

--- src/components/devis/Devis-form.jsx
+++ src/components/devis/Devis-form.jsx
@@ -1,3 +1,115 @@
-export default function DevisForm(){
+import React from 'react';
 
+function DevisForm() {
+    return (
+        <div className="container">
+            <h1 className="my-5">Demande de devis</h1>
+            <div className="row mb-3">
+                <div className="col-md-6">
+                    <h4>Vos informations:</h4>
+                    <form>
+                        <div className="mb-3">
+                            <label htmlFor="contactName" className="form-label">Contact:</label>
+                            <input type="text" className="form-control" id="contactName" defaultValue="Joannie Roy" />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="company" className="form-label">Entreprise:</label>
+                            <input type="text" className="form-control" id="company" />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="address" className="form-label">Adresse:</label>
+                            <input type="text" className="form-control" id="address" />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="city" className="form-label">Ville:</label>
+                            <input type="text" className="form-control" id="city" />
+                        </div>
+                    </form>
+                </div>
+                <div className="col-md-6">
+                    <h4>Veuillez choisir par quel moyen vous souhaitez être contacté:</h4>
+                    <form>
+                        <div className="form-check">
+                            <input type="radio" className="form-check-input" name="contactMethod" id="contactPhone" />
+                            <label className="form-check-label" htmlFor="contactPhone">Téléphone</label>
+                        </div>
+                        <div className="form-check">
+                            <input type="radio" className="form-check-input" name="contactMethod" id="contactEmail" defaultChecked />
+                            <label className="form-check-label" htmlFor="contactEmail">Courriel</label>
+                        </div>
+                        <div className="mb-3">
+                            <input type="email" className="form-control" id="emailAddress" defaultValue="joebloe@entreprise.com" />
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+            <table className="table table-bordered">
+                <thead>
+                <tr>
+                    <th>Description</th>
+                    <th>Quantité</th>
+                    <th>Prix Unitaire</th>
+                    <th>Coût</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>ABB - ACSM1-04AM-012A-4</td>
+                    <td>
+                        <input type="number" className="form-control" defaultValue={1} min={0} />
+                    </td>
+                    <td>1850.00 $</td>
+                    <td>1850.00 $</td>
+                </tr>
+                <tr>
+                    <td>ABB - ACSM1-04AM-012A-4</td>
+                    <td>
+                        <input type="number" className="form-control" defaultValue={2} min={0} />
+                    </td>
+                    <td>1850.00 $</td>
+                    <td>3700.00 $</td>
+                </tr>
+                <tr>
+                    <td>ABB - ACSM1-04AM-012A-4</td>
+                    <td>
+                        <input type="number" className="form-control" defaultValue={1} min={0} />
+                    </td>
+                    <td>1850.00 $</td>
+                    <td>1850.00 $</td>
+                </tr>
+                <tr>
+                    <td>ABB - ACSM1-04AM-012A-4</td>
+                    <td>
+                        <input type="number" className="form-control w-50" defaultValue={1} min={0} />
+                    </td>
+                    <td>1850.00 $</td>
+                    <td>1850.00 $</td>
+                </tr>
+                </tbody>
+                <tfoot>
+                <tr>
+                    <td colSpan={3} className="text-end">Sous-total:</td>
+                    <td>185 000.00 $</td>
+                </tr>
+                <tr>
+                    <td colSpan={3} className="text-end">Taxes:</td>
+                    <td>2000.00 $</td>
+                </tr>
+                <tr>
+                    <td colSpan={3} className="text-end">Total:</td>
+                    <td>187 000.00 $</td>
+                </tr>
+                </tfoot>
+            </table>
+
+            <div className="d-flex justify-content-end mt-5 ">
+                <button type="button" className="btn btn-dark btn">
+                    Envoyer la demande
+                </button>
+            </div>
+        </div>
+    );
 }
+
+export default DevisForm;
