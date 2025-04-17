3961a0f2e7ed6f8fef14596708d87825ef3d876e
Janelle <2264502@cegeplimoilou.ca>
2025-04-17 15:02:21 -0400

    Modification style formulaire connexion

--- src/components/authentification/Connexion.jsx
+++ src/components/authentification/Connexion.jsx
@@ -19,13 +19,13 @@ export default function Connexion() {
     };
 
     return (
-        <div className="container-fluid min-vh-100 d-flex align-items-center justify-content-center">
-            <div className="row w-100 shadow-lg rounded overflow-hidden border border-2" style={{ maxWidth: '1000px' }}>
+        <div className="container-fluid min-vh-100 d-flex align-items-start justify-content-center">
+            <div className="row w-100 shadow-lg rounded overflow-hidden border border-2 marge-custom-connexion" style={{ maxWidth: '1000px' }}>
                 <div className="col-md-5 d-flex justify-content-center align-items-center bg-bleu-fonce1">
                     <img src={logo} alt="Logo multifd" className="w-100 p-4" style={{ maxHeight: "90%" }} />
                 </div>
 
-                <div className="col-md-7 bg-light p-4 text-black">
+                <div className="col-md-7 bg-light p-4 text-black p-5">
                     <h3 className="text-center mb-3 texte-bleu-fonce1">
                         <strong>Connexion à votre compte</strong>
                     </h3>
