0d8ffa36719f40fd19f8927d9c9f4764f0e3207a
Janelle <2264502@cegeplimoilou.ca>
2025-05-06 14:15:04 -0400

    Ajout accolade manquante

--- src/scripts/http.js
+++ src/scripts/http.js
@@ -9,218 +9,218 @@ export async function fetchTexteClient() {
     return await response.text();
 
 
-/**
- * Ajout random pour forcer un build hihihiihih vous pouvez effacer ce message si vous le voyez
- * Récupère tous les produits du backend
- */
-export async function fetchAllProduits() {
-    const response = await fetch(BASE_URL + "/toutProduits", {
-        method: "GET",
-        headers: {"Content-type": "application/json"}
-    });
-    if (!response.ok) {
-        throw new Error("Erreur lors du chargement des produits.");
-    }
-    return await response.json();
-}
-
-/**
- * Récupère un produit par son ID
- * @param id identifiant du produit
- */
-export async function fetchProduitParId(id) {
-    const response = await fetch(BASE_URL + "/produit/" + id, {
-        method: "GET",
-        headers: {"Content-type": "application/json"}
-    });
-    if (!response.ok) {
-        throw new Error("Erreur lors du chargement du produit avec ID: " + id);
-    }
-    return await response.json();
-}
-
-/**
- * Ajoute un nouveau produit au backend
- * @param produit les données du produit à ajouter
- */
-export async function ajouterProduit(produit) {
-    console.log(produit);
-    let nouveau = {
-        nom: produit.nom,
-        disponible: produit.disponible,
-        prix: parseInt(produit.prix),
-        etat: produit.etat,
-        poids: parseInt(produit.poids),
-        marque: {nom: produit.marque},
-        voltage: parseInt(produit.voltage),
-        amperage: parseInt(produit.amperage),
-        hp: parseInt(produit.hp),
-        courant: parseInt(produit.courant)
-    };
-
-    const response = await fetch(BASE_URL + "/admin/ajouteProduit", {
-        method: 'POST',
-        body: JSON.stringify(nouveau),
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + sessionStorage.getItem("token")
+    /**
+     * Ajout random pour forcer un build hihihiihih vous pouvez effacer ce message si vous le voyez
+     * Récupère tous les produits du backend
+     */
+    export async function fetchAllProduits() {
+        const response = await fetch(BASE_URL + "/toutProduits", {
+            method: "GET",
+            headers: {"Content-type": "application/json"}
+        });
+        if (!response.ok) {
+            throw new Error("Erreur lors du chargement des produits.");
         }
-    });
-    if (!response.ok) {
-        throw new Error("Erreur lors de l'ajout du produit.");
+        return await response.json();
     }
 
-    return response;
-}
-
-/**
- * Modifie un produit existant par ID
- * @param id identifiant du produit à modifier
- * @param produit les nouvelles données du produit
- */
-export async function modifierProduit(id, produit) {
-    const response = await fetch(BASE_URL + "/admin/" + id, {
-        method: 'PUT',
-        body: JSON.stringify(produit),
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + sessionStorage.getItem("token")
+    /**
+     * Récupère un produit par son ID
+     * @param id identifiant du produit
+     */
+    export async function fetchProduitParId(id) {
+        const response = await fetch(BASE_URL + "/produit/" + id, {
+            method: "GET",
+            headers: {"Content-type": "application/json"}
+        });
+        if (!response.ok) {
+            throw new Error("Erreur lors du chargement du produit avec ID: " + id);
         }
-    });
-    if (!response.ok) {
-        throw new Error("Erreur lors de la modification du produit ID: " + id);
-    }
-    return await response.json();
-}
-
-/**
- * Supprime un produit par ID
- * @param id identifiant du produit à supprimer
- */
-export async function supprimerProduit(id) {
-    const response = await fetch(BASE_URL + "/" + id, {
-        method: 'DELETE'
-    });
-    if (!response.ok) {
-        throw new Error("Erreur lors de la suppression du produit ID: " + id);
-    }
-}
-
-export async function fetchUtilisateur(email) {
-    const response = await fetch(BASE_URL + "/client/info/" + email, {
-        method: "GET",
-        headers: {
-            "Content-type": "application/json",
-            'Authorization': 'Bearer ' + sessionStorage.getItem("token")
-        }
-    });
-    if (!response.ok) {
-        throw new Error("Erreur lors de la récupération de l'utilisateur " + email);
-    }
-    return await response.json();
-}
-
-/**
- * Modifie un produit existant par ID
- * @param id identifiant du produit à modifier
- * @param utilisateur les nouvelles données du produit
- */
-export async function modifierUtilisateur(email, utilisateur) {
-    const response = await fetch(BASE_URL + "/client/modifier/" + email, {
-        method: 'PUT',
-        body: JSON.stringify(utilisateur),
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + sessionStorage.getItem("token")
-        }
-    });
-    if (!response.ok) {
-        throw new Error("Erreur lors de la modification de l'utilisateur " + email);
-    }
-    return await response.json();
-}
-
-export async function inscription(formData) {
-    const donnee = {
-        mail: formData.email,
-        entreprise: formData.company,
-        mdp: formData.password,
-        nom: formData.lastName,
-        prenom: formData.firstName
-    };
-    return fetch(BASE_URL + '/inscription', {
-        method: 'POST',
-        headers: {
-            'Content-type': 'application/json'
-        },
-        body: JSON.stringify(donnee)
-    })
-}
-
-export async function connexion(formData, setError) {
-    const response = await fetch(BASE_URL + '/connexion', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(formData)
-    });
-
-    if (!response.ok) {
-        if (response.status === 400) {
-            setError({error: "Erreur authentification", message: "le courriel ou le mot de passe ne sont pas bon"});
-        }
-        return null;
+        return await response.json();
     }
 
-    const data = await response.json();
-    //todo mettre dans cookie ici ou dans composant connexion
-    sessionStorage.setItem("token", data.bearer);
-    sessionStorage.setItem("mail", data.mail);
-    sessionStorage.setItem("role", data.role);
-    sessionStorage.setItem("isConnected", true);
-    sessionStorage.setItem("nom", data.nom);
-    sessionStorage.setItem("prenom", data.prenom);
-    return data; // Retourne les informations utilisateur
-}
+    /**
+     * Ajoute un nouveau produit au backend
+     * @param produit les données du produit à ajouter
+     */
+    export async function ajouterProduit(produit) {
+        console.log(produit);
+        let nouveau = {
+            nom: produit.nom,
+            disponible: produit.disponible,
+            prix: parseInt(produit.prix),
+            etat: produit.etat,
+            poids: parseInt(produit.poids),
+            marque: {nom: produit.marque},
+            voltage: parseInt(produit.voltage),
+            amperage: parseInt(produit.amperage),
+            hp: parseInt(produit.hp),
+            courant: parseInt(produit.courant)
+        };
 
-export function fetchProduitFilter(motCle) {
-    return fetch(BASE_URL + "/rechercheProduit?nomFiltre=" + motCle, {
-        method: "GET",
-        headers: {"Content-type": "application/json"}
-    })
-        .then((r) => {
-            if (r.ok) {
-                return r.json();
-            } else {
-                throw new Error("Erreur réseau");
+        const response = await fetch(BASE_URL + "/admin/ajouteProduit", {
+            method: 'POST',
+            body: JSON.stringify(nouveau),
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + sessionStorage.getItem("token")
             }
         });
-}
-
-export async function fetchAllMarque() {
-    const response = await fetch(BASE_URL + "/marques", {
-        method: "GET",
-        headers: {"Content-type": "application/json"}
-    });
-    if (!response.ok) {
-        throw new Error("Erreur lors du chargement des marques.");
-    }
-    return await response.json();
-}
-
-
-
-export async function fetchAllUtilisateurs() {
-    const response = await fetch(BASE_URL + "/admin/utilisateurs", {
-        method: "GET",
-        headers: {
-            "Content-type": "application/json",
-
+        if (!response.ok) {
+            throw new Error("Erreur lors de l'ajout du produit.");
         }
-    });
-    if (!response.ok) {
-        throw new Error("Erreur lors du chargement des utilisateurs.");
+
+        return response;
     }
-    return await response.json();
+
+    /**
+     * Modifie un produit existant par ID
+     * @param id identifiant du produit à modifier
+     * @param produit les nouvelles données du produit
+     */
+    export async function modifierProduit(id, produit) {
+        const response = await fetch(BASE_URL + "/admin/" + id, {
+            method: 'PUT',
+            body: JSON.stringify(produit),
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + sessionStorage.getItem("token")
+            }
+        });
+        if (!response.ok) {
+            throw new Error("Erreur lors de la modification du produit ID: " + id);
+        }
+        return await response.json();
+    }
+
+    /**
+     * Supprime un produit par ID
+     * @param id identifiant du produit à supprimer
+     */
+    export async function supprimerProduit(id) {
+        const response = await fetch(BASE_URL + "/" + id, {
+            method: 'DELETE'
+        });
+        if (!response.ok) {
+            throw new Error("Erreur lors de la suppression du produit ID: " + id);
+        }
+    }
+
+    export async function fetchUtilisateur(email) {
+        const response = await fetch(BASE_URL + "/client/info/" + email, {
+            method: "GET",
+            headers: {
+                "Content-type": "application/json",
+                'Authorization': 'Bearer ' + sessionStorage.getItem("token")
+            }
+        });
+        if (!response.ok) {
+            throw new Error("Erreur lors de la récupération de l'utilisateur " + email);
+        }
+        return await response.json();
+    }
+
+    /**
+     * Modifie un produit existant par ID
+     * @param id identifiant du produit à modifier
+     * @param utilisateur les nouvelles données du produit
+     */
+    export async function modifierUtilisateur(email, utilisateur) {
+        const response = await fetch(BASE_URL + "/client/modifier/" + email, {
+            method: 'PUT',
+            body: JSON.stringify(utilisateur),
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + sessionStorage.getItem("token")
+            }
+        });
+        if (!response.ok) {
+            throw new Error("Erreur lors de la modification de l'utilisateur " + email);
+        }
+        return await response.json();
+    }
+
+    export async function inscription(formData) {
+        const donnee = {
+            mail: formData.email,
+            entreprise: formData.company,
+            mdp: formData.password,
+            nom: formData.lastName,
+            prenom: formData.firstName
+        };
+        return fetch(BASE_URL + '/inscription', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(donnee)
+        })
+    }
+
+    export async function connexion(formData, setError) {
+        const response = await fetch(BASE_URL + '/connexion', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formData)
+        });
+
+        if (!response.ok) {
+            if (response.status === 400) {
+                setError({error: "Erreur authentification", message: "le courriel ou le mot de passe ne sont pas bon"});
+            }
+            return null;
+        }
+
+        const data = await response.json();
+        //todo mettre dans cookie ici ou dans composant connexion
+        sessionStorage.setItem("token", data.bearer);
+        sessionStorage.setItem("mail", data.mail);
+        sessionStorage.setItem("role", data.role);
+        sessionStorage.setItem("isConnected", true);
+        sessionStorage.setItem("nom", data.nom);
+        sessionStorage.setItem("prenom", data.prenom);
+        return data; // Retourne les informations utilisateur
+    }
+
+    export function fetchProduitFilter(motCle) {
+        return fetch(BASE_URL + "/rechercheProduit?nomFiltre=" + motCle, {
+            method: "GET",
+            headers: {"Content-type": "application/json"}
+        })
+            .then((r) => {
+                if (r.ok) {
+                    return r.json();
+                } else {
+                    throw new Error("Erreur réseau");
+                }
+            });
+    }
+
+    export async function fetchAllMarque() {
+        const response = await fetch(BASE_URL + "/marques", {
+            method: "GET",
+            headers: {"Content-type": "application/json"}
+        });
+        if (!response.ok) {
+            throw new Error("Erreur lors du chargement des marques.");
+        }
+        return await response.json();
+    }
+
+
+    export async function fetchAllUtilisateurs() {
+        const response = await fetch(BASE_URL + "/admin/utilisateurs", {
+            method: "GET",
+            headers: {
+                "Content-type": "application/json",
+
+            }
+        });
+        if (!response.ok) {
+            throw new Error("Erreur lors du chargement des utilisateurs.");
+        }
+        return await response.json();
+    }
 }
