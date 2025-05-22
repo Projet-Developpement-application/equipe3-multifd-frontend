2206b603655e8894b157827a9977ad715184add0
Janelle <2264502@cegeplimoilou.ca>
2025-05-22 12:58:04 -0400

    Test changement route

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,7 @@ import {useEffect, useState} from "react";
 import ModifierProduit from "./components/gestion-produits/Modifier-Produit-adm.jsx";
 import SupprimerProduit from "./components/gestion-produits/Supprimer-Produit-adm.jsx";
 import UtilisateurAdm from "./components/gestion-comptes/UtilisateurAdm.jsx";
- export const URL_ROUTE_FRONTEND = "/siteReact/equipe3-multifd-frontend";
-//export const URL_ROUTE_FRONTEND = "";
+export const URL_ROUTE_FRONTEND = "/siteReact/equipe3-multifd-frontend/";
 export const URL_BACKEND = "http://localhost:8080";
 
 function App() {
@@ -40,7 +39,7 @@ function App() {
     return (
         <>
             <UtilisateurContext.Provider value={{utilisateur,setUtilisateur}}>
-                <BrowserRouter>
+                <BrowserRouter basename="/siteReact/equipe3-multifd-frontend">
                     <Navbar/>
 
                     <Routes>
@@ -50,16 +49,16 @@ function App() {
                             </>
                         }/>
 
-                        <Route path={URL_ROUTE_FRONTEND+"compte"} element={<Compte/>}/>
-                        <Route path={URL_ROUTE_FRONTEND+"panier"} element={<DevisForm/>}/>
-                        <Route path={URL_ROUTE_FRONTEND+"produit/:id"} element={<Produit/>}/>
-                        <Route path={URL_ROUTE_FRONTEND+"AjouterProduit"} element={<AjouterProduit/>}/>
-                        <Route path={URL_ROUTE_FRONTEND+"ModifierProduit/:id"} element={<ModifierProduit/>}/>
-                        <Route path={URL_ROUTE_FRONTEND+"SupprimerProduit/:id"} element={<SupprimerProduit/>}/>
-                        <Route path={URL_ROUTE_FRONTEND+"Inscription"} element={<Inscription/>}/>
-                        <Route path={URL_ROUTE_FRONTEND+"Connexion"} element={<Connexion/>}/>
-                        <Route path={URL_ROUTE_FRONTEND+"GestionUtilisateur"} element={<GestionUtilisateurAdm/>}/>
-                        <Route path={URL_ROUTE_FRONTEND+"utilisateur/:mail"} element={<UtilisateurAdm />} />
+                        {/*<Route path={URL_ROUTE_FRONTEND+"compte"} element={<Compte/>}/>*/}
+                        {/*<Route path={URL_ROUTE_FRONTEND+"panier"} element={<DevisForm/>}/>*/}
+                        {/*<Route path={URL_ROUTE_FRONTEND+"produit/:id"} element={<Produit/>}/>*/}
+                        {/*<Route path={URL_ROUTE_FRONTEND+"AjouterProduit"} element={<AjouterProduit/>}/>*/}
+                        {/*<Route path={URL_ROUTE_FRONTEND+"ModifierProduit/:id"} element={<ModifierProduit/>}/>*/}
+                        {/*<Route path={URL_ROUTE_FRONTEND+"SupprimerProduit/:id"} element={<SupprimerProduit/>}/>*/}
+                        {/*<Route path={URL_ROUTE_FRONTEND+"Inscription"} element={<Inscription/>}/>*/}
+                        {/*<Route path="Connexion" element={<Connexion/>}/>
+                        <Route path={URL_ROUTE_FRONTEND+"GestionUtilisateur"} element={<GestionUtilisateurAdm/>}/>*/}
+                        {/*<Route path={URL_ROUTE_FRONTEND+"utilisateur/:mail"} element={<UtilisateurAdm />} />*/}
 
                     </Routes>
 
