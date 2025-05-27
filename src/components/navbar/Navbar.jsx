02b8a8f315c76d4209b3127f0a6486cd25a58bb0
Janelle <2264502@cegeplimoilou.ca>
2025-05-27 15:56:20 -0400

    Changement bonjour navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import {useContext} from "react";
 import {URL_ROUTE_FRONTEND} from "../../App.jsx";
 
 export default function Navbar() {
-    const {utilisateur,setUtilisateur} = useContext(UtilisateurContext);
+    const {utilisateur, setUtilisateur} = useContext(UtilisateurContext);
     const navigate = useNavigate();
 
     /**
@@ -14,85 +14,75 @@ export default function Navbar() {
     const handleLogout = () => {
         sessionStorage.clear();
         setUtilisateur({
-            mail: null,
-            role: null,
-            nom: '',
-            prenom: '',
-            entreprise: ''
+            mail: null, role: null, nom: '', prenom: '', entreprise: ''
         })
 
-        navigate(URL_ROUTE_FRONTEND+"/Connexion");
+        navigate(URL_ROUTE_FRONTEND + "/Connexion");
     };
 
-    return (
-        <>
+    return (<>
             <RetourSiteBar/>
             <nav className="navbar w-100 z-1 navbar-expand-lg text-white bg-dark" data-bs-theme="dark">
                 <div className="container-fluid d-flex justify-content-between align-items-start">
-                    <Link className="navbar-brand navbar-logo ps-5" to={URL_ROUTE_FRONTEND+"/"}>
+                    <Link className="navbar-brand navbar-logo ps-5" to={URL_ROUTE_FRONTEND + "/"}>
                         MULTI-FD
                     </Link>
 
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav ms-auto">
-                            {utilisateur.mail === null ? (
-                                    /*utilisateur non connecté*/
+                            {utilisateur.mail === null ? (/*utilisateur non connecté*/
+                                <li className="nav-item me-4">
+                                    <Link to={URL_ROUTE_FRONTEND + "/Connexion"}
+                                          className="text-uppercase nav-link text-white hover-underline-animation left pb-1">
+                                        <i className="bi bi-person me-2"></i> Connexion
+                                    </Link>
+                                </li>) : utilisateur.role === "ADMIN" ? (/*utilisateur connecté comme administrateur*/
+                                <>
                                     <li className="nav-item me-4">
-                                        <Link to={URL_ROUTE_FRONTEND+"/Connexion"}
+                                        <Link to={URL_ROUTE_FRONTEND + "/GestionUtilisateur"}
                                               className="text-uppercase nav-link text-white hover-underline-animation left pb-1">
-                                            <i className="bi bi-person me-2"></i> Connexion
+                                            <i className="bi bi-people me-2"></i> Gestion Utilisateur
                                         </Link>
                                     </li>
-                                ) :
-                                utilisateur.role === "ADMIN" ? (
-                                    /*utilisateur connecté comme administrateur*/
-                                    <>
-                                        <li className="nav-item me-4">
-                                            <Link to={URL_ROUTE_FRONTEND+"/GestionUtilisateur"}
-                                                  className="text-uppercase nav-link text-white hover-underline-animation left pb-1">
-                                                <i className="bi bi-people me-2"></i> Gestion Utilisateur
-                                            </Link>
-                                        </li>
-                                        <li className="nav-item me-4">
-                                            <Link to={URL_ROUTE_FRONTEND+"/AjouterProduit"}
-                                                  className="text-uppercase nav-link text-white hover-underline-animation left pb-1">
-                                                <i className="bi bi-plus-circle me-2"></i> Ajouter un Produit
-                                            </Link>
-                                        </li>
+                                    <li className="nav-item me-4">
+                                        <Link to={URL_ROUTE_FRONTEND + "/AjouterProduit"}
+                                              className="text-uppercase nav-link text-white hover-underline-animation left pb-1">
+                                            <i className="bi bi-plus-circle me-2"></i> Ajouter un Produit
+                                        </Link>
+                                    </li>
 
-                                        <li className="nav-item me-4">
-                                            <button onClick={handleLogout} className="btn text-uppercase">
-                                                <i className="bi bi-box-arrow-left"></i>
-                                            </button>
-                                        </li>
-                                    </>
-                                ) : utilisateur.role === "CLIENT" ? (
-                                    /*utilisateur connnecté comme client*/
-                                    <>
-                                        <li className="nav-item me-4">
+                                    <li className="nav-item me-4">
+                                        <button onClick={handleLogout} className="btn text-uppercase">
+                                            <i className="bi bi-box-arrow-left"></i>
+                                        </button>
+                                    </li>
+                                </>) : utilisateur.role === "CLIENT" ? (/*utilisateur connnecté comme client*/
+                                <>
+                                    <li className="nav-item me-4">
+                                        <div
+                                            className="text-uppercase nav-link text-white  left pb-1">
+                                            <i className="bi bi-emoji-smile me-2"></i> Bonjour {utilisateur.prenom}
+                                        </div>
 
-                                            <i className="bi bi-person me-2"></i> Bonjour {utilisateur.prenom}
-
-                                        </li>
-                                        <li className="nav-item me-4">
-                                            <Link to={URL_ROUTE_FRONTEND+"/compte"}
-                                                  className="text-uppercase nav-link text-white hover-underline-animation left pb-1">
-                                                <i className="bi bi-person me-2"></i> Mon Compte
-                                            </Link>
-                                        </li>
-                                        <li className="nav-item me-4">
-                                            <Link to={URL_ROUTE_FRONTEND+"/panier"}
-                                                  className="text-uppercase nav-link text-white hover-underline-animation left pb-1">
-                                                <i className="bi bi-cart me-2"></i> Panier
-                                            </Link>
-                                        </li>
-                                        <li className="nav-item me-4">
-                                            <button onClick={handleLogout} className="btn text-uppercase">
-                                                <i className="bi bi-box-arrow-left"></i>
-                                            </button>
-                                        </li>
-                                    </>
-                                ) : undefined}
+                                    </li>
+                                    <li className="nav-item me-4">
+                                        <Link to={URL_ROUTE_FRONTEND + "/compte"}
+                                              className="text-uppercase nav-link text-white hover-underline-animation left pb-1">
+                                            <i className="bi bi-person me-2"></i> Mon Compte
+                                        </Link>
+                                    </li>
+                                    <li className="nav-item me-4">
+                                        <Link to={URL_ROUTE_FRONTEND + "/panier"}
+                                              className="text-uppercase nav-link text-white hover-underline-animation left pb-1">
+                                            <i className="bi bi-cart me-2"></i> Panier
+                                        </Link>
+                                    </li>
+                                    <li className="nav-item me-4">
+                                        <button onClick={handleLogout} className="btn text-uppercase">
+                                            <i className="bi bi-box-arrow-left"></i>
+                                        </button>
+                                    </li>
+                                </>) : undefined}
                         </ul>
                     </div>
 
@@ -105,13 +95,10 @@ export default function Navbar() {
             </nav>
 
             {/* Barre rouge pour l'administrateur */}
-            {utilisateur.role === "ADMIN" && (
-                <div className="w-100 bg-danger text-white text-center py-2">
+            {utilisateur.role === "ADMIN" && (<div className="w-100 bg-danger text-white text-center py-2">
                     Connecté Administrateur
-                </div>
-            )}
+                </div>)}
 
             <Outlet/>
-        </>
-    );
+        </>);
 }
