b60cdaffa47b5aea59ef43c5096a2afbd14753ea
Janelle <2264502@cegeplimoilou.ca>
2025-05-29 12:37:42 -0400

    Style barre de filtres

--- src/components/catalogue/Sidebar.jsx
+++ src/components/catalogue/Sidebar.jsx
@@ -41,13 +41,14 @@ const Sidebar = ({ouvert, fermeture, filtres, setFiltres}) => {
 
     return (
         <div className={`sidebar ${ouvert ? "open" : ""}`}>
-            <div className="sidebar-header mb-5">
-                <h5>Filtres</h5>
+            <div className="sidebar-header mb-4">
+                <h5></h5>
                 <button className="btn-close" onClick={fermeture}></button>
             </div>
-            <div className="sidebar-content ps-2">
-                <div className="mb-5">
-                    <h6>Trier par</h6>
+            <div className="sidebar-content ">
+                <div className="mb-2">
+                    <h6 className="pb-2 "> Trier par :</h6>
+                    <div className="ps-2">
                     <FiltreRadio valeur={"asc"} name={"prixAsc"} identifiant={"sortPriceAsc"}
                                  motAffiche={"Prix croissant"} type={true} togglefiltre={toggleFiltre} filtres={filtres}
                     />
@@ -55,9 +56,11 @@ const Sidebar = ({ouvert, fermeture, filtres, setFiltres}) => {
                                  motAffiche={"Prix décroissant"} type={false} togglefiltre={toggleFiltre}
                                  filtres={filtres}
                     />
+                    </div>
                 </div>
+                <hr/>
+                <h5 className="py-4 "> Filtres :</h5>
 
-                <h6 className="mt-4 mb-4">Filtrer</h6>
 
                 {/* Ampérage */}
                 {/*<div className="filter-section mb-4">*/}
@@ -82,8 +85,8 @@ const Sidebar = ({ouvert, fermeture, filtres, setFiltres}) => {
 
             {/* Voltage */}
             <div className="filter-section mb-4">
-                <span>Voltage</span>
-                <div className="filter-options">
+                <h6 className="pb-2 "><i className="bi bi-caret-right-fill"></i> Voltage</h6>
+                <div className="filter-options ps-2">
                     {[600, 460, 240, 120].map((v) => (
                         <FiltreMulti key={v} typeFiltre={"voltage"} valeur={v} unite={"V"} toggleFiltre={toggleFiltre}
                                      filtres={filtres}/>
@@ -91,48 +94,52 @@ const Sidebar = ({ouvert, fermeture, filtres, setFiltres}) => {
                 </div>
 
             </div>
+            <hr/>
 
             {/* Marque */}
             <div className="filter-section mb-4">
-                <span>Marque</span>
-                <div className="filter-options">
+                <h6 className="pb-2 "><i class="bi bi-caret-right-fill"></i> Marque</h6>
+                <div className="filter-options ps-2">
                     {marques.map((marque) => (
                         <FiltreMulti key={marque.id} typeFiltre={"marques"} valeur={marque.nom}
                                      toggleFiltre={toggleFiltre} filtres={filtres}/>
                     ))}
                 </div>
             </div>
+            <hr/>
 
             {/* HP */}
             <div className="filter-section mb-4">
 
-                <span>HP</span>
+                <h6 className="pb-2 "> <i class="bi bi-caret-right-fill"></i> HP</h6>
 
-                <div className="filter-options">
+                <div className="filter-options ps-2">
                     {[1, 2, 3, 5, 7.5, 10, 15, 20, 40, 50, 100].map((v) => (
                         <FiltreMulti key={v} typeFiltre={"hp"} valeur={v} unite={"hp"} toggleFiltre={toggleFiltre}
                                      filtres={filtres}/>
                     ))}
                 </div>
             </div>
+            <hr/>
 
             {/* Disponibilité */}
             <div className="filter-section mb-4">
 
-                <span>Disponibilité</span>
+                <h6 className="pb-2 "><i className="bi bi-caret-right-fill"></i> Disponibilité </h6>
 
-                <div className="filter-options">
+                <div className="filter-options ps-2">
                     <FiltreMulti typeFiltre={"disponibilite"} valeur={true} toggleFiltre={toggleFiltre}
                                  filtres={filtres} unite={'Disponible'}/>
                     <FiltreMulti typeFiltre={"disponibilite"} valeur={false} toggleFiltre={toggleFiltre}
                                  filtres={filtres} unite={'Indisponible'}/>
                 </div>
             </div>
+            <hr/>
 
             {/* État */}
             <div className="filter-section mb-4">
-                <span>État</span>
-                <div className="filter-options">
+                <h6 className="pb-2 "><i className="bi bi-caret-right-fill"></i> État</h6>
+                <div className="filter-options ps-2">
                     {['NEUF', 'RECONDITIONNE', 'OCCASION'].map((v) => (
                         <FiltreMulti key={v} typeFiltre={"etat"} valeur={v} toggleFiltre={toggleFiltre}
                                      filtres={filtres}/>
